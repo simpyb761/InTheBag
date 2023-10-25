15259b85dc6bccd268bd54f80bc21151a2973587
simpyb761 <simpyb761@gmail.com>
2023-10-25 19:38:48 -0400

    Completed Program

---
+++ InTheBag-master/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using InTheBag.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace InTheBag.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+       public IActionResult IndexViewBag()
+        {
+            IList<string> Wishlist = new List<string>();
+            Wishlist.Add("Peace");
+            Wishlist.Add("Health");
+            Wishlist.Add("Happiness");
+            ViewBag.Wishlist = Wishlist;
+            return View();
+        }
+        public IActionResult IndexViewData()
+        {
+            IList<string> WishList = new List<string>();
+            WishList.Add("Quies");
+            WishList.Add("Salutem");
+            WishList.Add("Beatitudinem");
+            ViewData["WishList"] = WishList;
+            return View();
+        }
+        public IActionResult IndexTempData()
+        {
+            IList<string> WishList = new List<string>();
+            WishList.Add("La Paz");
+            WishList.Add(" La Salud");
+            WishList.Add("La Felicidad");
+            TempData["WishList"] = WishList;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
